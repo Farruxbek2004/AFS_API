fc2803d9772e40e75e1873d4ca020b8e9cffef86
Yunusboyev Farruxbek <farruh@yunusboyev.com>
2023-06-02 21:12:32 +0500

    no commit

--- Category/models.py
+++ Category/models.py
@@ -26,5 +26,3 @@ class Author(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
 
-    def __str__(self):
-        return self.name
